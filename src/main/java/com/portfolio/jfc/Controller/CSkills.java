d7b4778fc5854b4a847ed8caad056cdb8b86d831
JoseCabrera2022 <110561140+JoseCabrera2022@users.noreply.github.com>
2023-04-24 17:20:59 -0300

    modif paths

--- src/main/java/com/portfolio/jfc/Controller/CSkills.java
+++ src/main/java/com/portfolio/jfc/Controller/CSkills.java
@@ -27,7 +27,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/skill")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://portfolio-frontend-c724e.web.app")
 public class CSkills {
     @Autowired
     SSkills sSkills;
